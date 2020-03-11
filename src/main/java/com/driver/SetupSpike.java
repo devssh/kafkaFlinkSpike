2fb697f70c3a0bc1213b238195953da148f44a7d
Devashish Sood <devasood@gmail.com>
2020-03-11 16:06:54 +0530

    Add comments to Setup Spike for clarity

--- src/main/java/com/driver/SetupSpike.java
+++ src/main/java/com/driver/SetupSpike.java
@@ -33,7 +33,7 @@ public class SetupSpike {
 
         DataStream<String> txns = env.fromCollection(txns1).map(Transaction::toJSONString);
         sinkStream("transactions1", txns, env, properties);
-        System.out.println("sunk successfully");
+        System.out.println("sunk txns successfully");
         for (Transaction txn : txns1) {
             System.out.println(txn.toJSONString());
         }
@@ -43,14 +43,28 @@ public class SetupSpike {
             add(new PosTxnReq(2, 123));
 //            add(new PosTxnReq(3, 131));
         }};
-//        DataStream<PosTxnReq> postxns = env.fromCollection(postxns1).keyBy(x->x.txnID);
+        DataStream<String> postxns = env.fromCollection(postxns1).map(PosTxnReq::toJSONString);
+        sinkStream("transactions2", postxns, env, properties);
+
+
+        System.out.println("sunk postxns successfully");
+        for (PosTxnReq txn : postxns1) {
+            System.out.println(txn.toJSONString());
+        }
+
+        //Merge streams logic - need to add delay using Flink Watermark or use Flink Async I/O for external Data Access
+        // or use RichCoFlatmap depending on use case
         DataStream<Transaction> txnStream = consumeStream("transactions1",
                 env, properties).map(Transaction::fromJSONString).keyBy(x -> x.txnID);
-        DataStream<PosTxnReq> postxns = consumeStream("transactions2",
+
+        DataStream<PosTxnReq> postxnStream = consumeStream("transactions2",
                 env, properties).map(PosTxnReq::fromJSONString).keyBy(x -> x.txnID);
-//        DataStream<Transaction> txnStream = env.fromCollection(txns1).keyBy(x->x.txnID);
+//        DataStream<Transaction> txnStream = env.fromCollection(txns1).keyBy(x->x.txnID); //way to read from collection
+//        DataStream<PosTxnReq> postxns = env.fromCollection(postxns1).keyBy(x->x.txnID); //way to read from collection
+
         System.out.println("streams");
-        DataStream<PosTxnExpanded> expandedDataStream = txnStream.join(postxns).where(new KeySelector<Transaction, Integer>() {
+
+        DataStream<PosTxnExpanded> expandedDataStream = txnStream.join(postxnStream).where(new KeySelector<Transaction, Integer>() {
             @Override
             public Integer getKey(Transaction value) throws Exception {
                 return value.txnID;
@@ -60,6 +74,7 @@ public class SetupSpike {
             public Integer getKey(PosTxnReq value) throws Exception {
                 return value.txnID;
             }
+            // need to set Flink WindowAssigner depending on use case and
 //        }).window(GlobalWindows.create())
 //        }).window(TumblingProcessingTimeWindows.of(Time.minutes(1)))
         }).window(GlobalWindows.create()).allowedLateness(Time.seconds(30)).apply(new JoinFunction<Transaction, PosTxnReq, PosTxnExpanded>() {
@@ -68,13 +83,10 @@ public class SetupSpike {
                 return new PosTxnExpanded(txn, req);
             }
         });
+        // Alternative approaches use case specific
 //        DataStream<PosTxnExpanded> posTxnExpanded = txnStream.connect(postxns).flatMap(new EnrichmentFunction());
 //        sinkStream("txn2", posTxnExpanded.map(PosTxnExpanded::toJSONString), env, properties);
 
-        System.out.println("output successful");
-        for (PosTxnReq txn : postxns1) {
-            System.out.println(txn.toJSONString());
-        }
 
         System.out.println("created expanded stream");
         idealScenario();
